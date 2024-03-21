32bf518f18111633e3f6e8efbf8fb74f328f6b7d
adalix <gumustolgahan@gmail.com>
2024-03-21 04:17:33 +0300

    renamed fetchdata to fetchPokemons

--- script/poke.js
+++ script/poke.js
@@ -1,12 +1,10 @@
-fetchData();
-async function fetchData(){
+async function fetchPokemons(){
 
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
 
     if(!response.ok) {
         throw new Error("Could not fetch the data")
     }
-
     const data =  await response.json();
     return data;
 }
