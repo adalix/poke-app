98163c90062ba6b68f2a2700336b1bb73daf3d65
adalix <gumustolgahan@gmail.com>
2024-04-02 02:42:52 +0300

    fixed: removed createPokemonCard function, added if statement for pokemons card background color's

--- script/app.js
+++ script/app.js
@@ -2,24 +2,35 @@ const container = document.querySelector(".container");
 
 let pokemons = [];
 
-function createPokemonCard(pokemon) {
-  console.log(pokemon["name"]);
-}
 function renderData() {
-  console.log("current pokemon data length: ", pokemons.length);
-  pokemons.forEach((p) => {
+
+  pokemons.forEach(async (p) => {
     const card = document.createElement("div");
     card.classList.add('card')
+    const details =  await fetchPokemons(p.url);
+    const imgParent = document.createElement('div');
+    imgParent.classList.add('img')
+    const img = document.createElement('img')
+    img.classList.add('pokemonImg');
+
+
+
+    if(details.types[0].type.name){
+      const name = details.types[0].type.name;
+      card.classList.add(name)
+    }
+    img.src = `${details.sprites.front_default}`;
+    img.alt = `${p.name}`
+
     card.innerHTML += `
     <div class="top-info">
         <p class="name">${p.name}</p>
-        <p class="type">${p.type}</p>
+        <p class="type">${details.types[0].type.name}</p>
     </div>
-    <div class="img">
-        <img src="https://placehold.co/200x200" alt="${p.name}">
-    </div>
-    <div class="bottom-info"></div>`;
-
+   
+  `;
+    card.appendChild(imgParent)
+    imgParent.appendChild(img)
     container.appendChild(card)
     });
 }
@@ -28,7 +39,7 @@ fetchPokemons()
   .then((data) => {
     console.log("data fetch from server: ", data);
     pokemons = data.results;
-    renderData();
+    return renderData();
   })
   .catch((err) => {
     console.log(err);
